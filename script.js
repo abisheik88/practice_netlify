d11db93f70351dcff03be6e7c23ebe5b209f0ef2
Abisheik <77277634+abisheik88@users.noreply.github.com>
2022-10-12 12:26:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,8 @@
-var label=createlabels("label","for","first name","Firstname");
-var br1=createlinebreak("br");
-var br9=createlinebreak("br");
-var input=createinput("input","text","firstname","id","firstname");
-var br2=createlinebreak("br");
-
-var label1=createlabels("label","for","middle name"," Middlename");
-var br3=createlinebreak("br");
-var input1=createinput("input","text","middlename","id","middl;ename");
-var br4=createlinebreak("br");
-
-var label2=createlabels("label","for","last name","Lastname");
-var br5=createlinebreak("br");
-var input2=createinput("input","text","lastname","id","lastname");
-var br6=createlinebreak("br");
-
-var password=createlabels("label","number","number","Phonenumber");
-var br7=createlinebreak("br");
-var passinput=createinput("input","type","Number","id","number");
-var br8=createlinebreak("br");
-
-
-document.body.append(label,br1,input,br2,label1,br3,input1,br4,label2,br5,input2,br6,password,br7,passinput,br8,br9);
-
-
-function createlabels(tagname,attrname,attrvalue,content){
-var ele=document.createElement(tagname);
-ele.setAttribute(attrname,attrvalue);
-ele.innerHTML=content;
-return ele;
-}
-function createinput(tagname,attrname,attrvalue,attrname1,attrvalue1){
-    var ele=document.createElement(tagname);
-    ele.setAttribute(attrname,attrvalue);
-    ele.setAttribute(attrname1,attrvalue1);
-    return ele;
-}
-function createlinebreak(tagname){
-var ele=document.createElement(tagname);
-return ele;
+function sub(){
+    var res=document.getElementById("ftext").value;
+    var res1=document.getElementById("mtext").value;
+    var res2=document.getElementById("ltext").value;
+    var res3=document.getElementById("email").value;
+    var res4=document.getElementById("pass").value;
+     console.log(res,res1,res2,res3,res4);
 }
