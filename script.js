421bd77b9ab7fede7cf9c39cf411c8a893cd4c3b
Abisheik <77277634+abisheik88@users.noreply.github.com>
2022-10-13 12:16:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function createtabledata(tagname,attrname,attrvalue,content){
+    var ele = document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    ele.innerHTML=content;
+    return ele;
+}
+function createtable(tagname,attrname,attrvalue){
+    var ele = document.createElement(tagname);
+    ele.setAttribute(attrname,attrvalue);
+    return ele;
+}
+function createtd(tagname,content){
+    var ele = document.createElement(tagname);
+    ele.innerHTML=content;
+    return ele;
+
+}
+function createtablerow(tagname){
+    var ele = document.createElement(tagname);
+    return ele;
+}
+
+var table = createtable("table","class","table");
+var tablehead = createtable("thead","class","thead-dark");
+var tablerow1= createtablerow("tr");
+var tableh1 = createtabledata("th","scope","col","First");
+var tableh12 = createtabledata("th","scope","col","First");
+var tableh13 = createtabledata("th","scope","col","First");
+var tablerow2= createtablerow("tr");
+var tablebody = createtablerow("tbody");
+var tableh2 = createtd("td","Abisheik");
+var tableh21 = createtd("td","mark");
+var tableh22 = createtd("td","vimal");
+table.append(tablehead);
+tablehead.append(tablerow1);
+tablerow1.append(tableh1);
+tablerow1.append(tableh12);
+tablerow1.append(tableh13);
+table.append(tablebody);
+tablebody.append(tablerow2);
+tablerow2.append(tableh2);
+tablerow2.append(tableh21);
+tablerow2.append(tableh22);
+document.body.append(table);
